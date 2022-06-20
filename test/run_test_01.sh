967685e628a9281e3c6d97c5b06aba5d3ce8ecee
MalcolmorianVII <bdkutambe@gmail.com>
2022-06-20 09:17:23 +0200

    Added python3 & added correct file path

--- test/run_test_01.sh
+++ test/run_test_01.sh
@@ -2,19 +2,19 @@ set -e
 set -o pipefail
 
 #rm app/pa_seqbox_v2_test.db
-python test/test_no_web.py # creates db
-python src/scripts/seqbox_cmd.py add_groups -i test/01.test_todo_list_query/groups.csv
-python src/scripts/seqbox_cmd.py add_projects -i test/01.test_todo_list_query/projects.csv
-python src/scripts/seqbox_cmd.py add_pcr_assays -i test/01.test_todo_list_query/pcr_assay.csv 
-python src/scripts/seqbox_cmd.py add_sample_sources -i test/01.test_todo_list_query/sample_sources.csv
-python src/scripts/seqbox_cmd.py add_samples -i test/01.test_todo_list_query/samples.csv
-python src/scripts/seqbox_cmd.py add_pcr_results -i test/01.test_todo_list_query/pcr_results.csv
-python src/scripts/seqbox_cmd.py add_extractions -i test/01.test_todo_list_query/extraction.csv
-python src/scripts/seqbox_cmd.py add_tiling_pcrs -i test/01.test_todo_list_query/tiling_pcr.csv
-python src/scripts/seqbox_cmd.py add_covid_confirmatory_pcrs -i test/01.test_todo_list_query/confirmatory_pcr.csv
-python src/scripts/seqbox_cmd.py add_raw_sequencing_batches -i test/01.test_todo_list_query/raw_sequencing_batch.csv
-python src/scripts/seqbox_cmd.py add_readset_batches -i test/01.test_todo_list_query/readset_batches.csv
-python src/scripts/seqbox_cmd.py add_readsets -i test/01.test_todo_list_query/nanopore_default_readsets.csv -s -n 
-python src/scripts/seqbox_filehandling.py add_readset_to_filestructure -i test/01.test_todo_list_query/nanopore_default_readsets.csv -c test/test_seqbox_config.yaml -s -n
-python src/scripts/seqbox_cmd.py add_artic_covid_results -i test/01.test_todo_list_query/artic_nf_covid_results.csv -b 20201201_1355_MN33881_FAO20804_109641e0 -w medaka -p docker
-python src/scripts/seqbox_cmd.py add_pangolin_results -i test/01.test_todo_list_query/pangolin_results.csv -b 20201201_1355_MN33881_FAO20804_109641e0 -w medaka -p docker
+python3 test_no_web.py # creates db
+python3 ../src/scripts/seqbox_cmd.py add_groups -i 01.test_todo_list_query/groups.csv
+python3 ../src/scripts/seqbox_cmd.py add_projects -i 01.test_todo_list_query/projects.csv
+python3 ../src/scripts/seqbox_cmd.py add_pcr_assays -i 01.test_todo_list_query/pcr_assay.csv 
+python3 ../src/scripts/seqbox_cmd.py add_sample_sources -i 01.test_todo_list_query/sample_sources.csv
+python3 ../src/scripts/seqbox_cmd.py add_samples -i 01.test_todo_list_query/samples.csv
+python3 ../src/scripts/seqbox_cmd.py add_pcr_results -i 01.test_todo_list_query/pcr_results.csv
+python3 ../src/scripts/seqbox_cmd.py add_extractions -i 01.test_todo_list_query/extraction.csv
+python3 ../src/scripts/seqbox_cmd.py add_tiling_pcrs -i 01.test_todo_list_query/tiling_pcr.csv
+python3 ../src/scripts/seqbox_cmd.py add_covid_confirmatory_pcrs -i 01.test_todo_list_query/confirmatory_pcr.csv
+python3 ../src/scripts/seqbox_cmd.py add_raw_sequencing_batches -i 01.test_todo_list_query/raw_sequencing_batch.csv
+python3 ../src/scripts/seqbox_cmd.py add_readset_batches -i 01.test_todo_list_query/readset_batches.csv
+python3 ../src/scripts/seqbox_cmd.py add_readsets -i 01.test_todo_list_query/nanopore_default_readsets.csv -s -n 
+python3 ../src/scripts/seqbox_filehandling.py add_readset_to_filestructure -i 01.test_todo_list_query/nanopore_default_readsets.csv -c test_seqbox_config.yaml -s -n
+python3 ../src/scripts/seqbox_cmd.py add_artic_covid_results -i 01.test_todo_list_query/artic_nf_covid_results.csv -b 20201201_1355_MN33881_FAO20804_109641e0 -w medaka -p docker
+python3 ../src/scripts/seqbox_cmd.py add_pangolin_results -i 01.test_todo_list_query/pangolin_results.csv -b 20201201_1355_MN33881_FAO20804_109641e0 -w medaka -p docker
