f10afa7fa207263bb8e7e51f413947e7cfbbdced
MalcolmorianVII <bdkutambe@gmail.com>
2022-06-20 09:25:56 +0200

    put sensitive info in env vars

--- test/test_no_web.py
+++ test/test_no_web.py
@@ -1,5 +1,6 @@
 import os
 from app import db
+from config import PASSWORD,PORT,USER,DB_POSTGRES
 
 
 def create_it():
@@ -8,8 +9,12 @@ def create_it():
 
 def wipe_it_and_create_it():
     # putting this assertion here to stop me from wiping the non-test database
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    #postgresql://user:password@localhost:5432/database_name
+
+    # SQLALCHEMY_DATABASE_URI = 'postgresql:///test_sq_service_db:'
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@localhost:{PORT}/{DB_POSTGRES}'
     assert SQLALCHEMY_DATABASE_URI.split('/')[-1].startswith('test')
+    
     db.drop_all()
     db.create_all()
 
