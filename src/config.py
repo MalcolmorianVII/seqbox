d925aeb69b9d54bc48200dbc717d459688f0c150
MalcolmorianVII <bdkutambe@gmail.com>
2022-06-20 09:13:24 +0200

    Added env vars

--- src/config.py
+++ src/config.py
@@ -1,6 +1,10 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+PASSWORD = os.environ['PASSWORD']
+USER = os.environ['USER']
+PORT = os.environ['PORT']
+DB_POSTGRES = os.environ['DB_POSTGRES']
 
 class Config(object):
     """[Configuration file]
@@ -8,6 +12,5 @@ class Config(object):
 
     # Flask-SQLAlchemy: Initialize
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///pa_seqbox_v2_test.db'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@localhost:{PORT}/{DB_POSTGRES}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
