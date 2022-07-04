fab242184c2480f9bf8bd0c8a6975f5e9ca1fd77
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2022-07-04 08:43:52 +0200

    format db env settings

--- src/config.py
+++ src/config.py
@@ -1,10 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-PASSWORD = os.environ['PASSWORD']
-USER = os.environ['USER']
-PORT = os.environ['PORT']
-DB_POSTGRES = os.environ['DB_POSTGRES']
 
 class Config(object):
     """[Configuration file]
@@ -12,5 +8,5 @@ class Config(object):
 
     # Flask-SQLAlchemy: Initialize
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@localhost:{PORT}/{DB_POSTGRES}'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
