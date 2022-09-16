7d7663f5c59260acd59ecbe6a9cf7442b98f8740
MalcolmorianVII <bdkutambe@gmail.com>
2022-09-16 14:48:50 +0200

    code refactor

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from app import app, db
-from app.models import User, Sample, Post, Batch, Location, Result1, Mykrobe, Project, Sample_project
-
+# from app.models import User, Sample, Post, Batch, Location, Result1, Mykrobe, Project, Sample_project
+from app.models import User, Sample, Mykrobe, Project
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Sample': Sample, 'Batch': Batch, 'Location': Location, 'Result1': Result1, 'Mykrobe': Mykrobe, 'Sample_project': Sample_project, 'Project': Project, 'Post': Post}
+    return {'db': db, 'User': User, 'Sample': Sample, 'Mykrobe': Mykrobe, 'Project': Project}
 
 if __name__ == "__main__":
     # this db.create_all() might just be needed for sqlite
